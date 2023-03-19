f9857b26afd59b07eb95e5f7e556b5577a245f93
jum0624 <jum0624@naver.com>
2023-03-20 00:09:42 +0900

    refactor: AlarmService 코드 예외관련 코드 리펙터링

--- doormate/src/main/java/com/doormate/service/impl/AlarmService.java
+++ doormate/src/main/java/com/doormate/service/impl/AlarmService.java
@@ -4,9 +4,10 @@ import com.doormate.domain.Alarm;
 import com.doormate.domain.Message;
 import com.doormate.domain.Reminder;
 import com.doormate.domain.RepetitionPeriod;
+import com.doormate.exception.NotFoundException;
 import com.doormate.repository.AlarmRepository;
 import com.doormate.repository.ReminderRepository;
-import com.doormate.util.RepetitionDate;
+import com.doormate.util.RepetitionDateHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,31 +21,37 @@ import java.util.List;
 public class AlarmService {
     private final ReminderRepository reminderRepository;
     private final AlarmRepository alarmRepository;
-    private final RepetitionDate repetitionDate;
+    private final RepetitionDateHandler repetitionDateHandler;
+
+    private static final String NOT_FIND_REMINDER_MESSAGE = "해당 리마인더가 존재하지 않습니다.";
+    private static final String SUCCESS_SAVED_ALARM_MESSAGE = "반복 설정 등록 완료";
 
     // === Alarm 테이블에 저장 === //
     @Transactional
-    public Message saveAlarm(Long id) {
-        Reminder reminder = reminderRepository.findById(id).orElse(null);
+    public String saveAlarm(Long id) {
+        Reminder reminder = reminderRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(NOT_FIND_REMINDER_MESSAGE));
+
         RepetitionPeriod repetitionPeriod = reminder.getRepetitionPeriod();
-        Message message;
 
         if (repetitionPeriod == RepetitionPeriod.DAILY) {
-            message = repetitionDate.saveDailyAlarm(id);
+            repetitionDateHandler.saveDailyAlarm(id);
         } else if (repetitionPeriod == RepetitionPeriod.WEEKLY) {
-            message = repetitionDate.saveWeeklyAlarm(id);
+            repetitionDateHandler.saveWeeklyAlarm(id);
         } else if (repetitionPeriod == RepetitionPeriod.MONTHLY) {
-            message = repetitionDate.saveMonthlyAlarm(id);
+            repetitionDateHandler.saveMonthlyAlarm(id);
         } else if (repetitionPeriod == RepetitionPeriod.YEARLY) {
-            message = repetitionDate.saveYearlyAlarm(id);
+            repetitionDateHandler.saveYearlyAlarm(id);
         } else {
-            message = repetitionDate.saveOneAlarm(id);
+            repetitionDateHandler.saveOneAlarm(id);
         }
-        return message;
+        return SUCCESS_SAVED_ALARM_MESSAGE;
     }
 
     @Transactional
     public void deleteAlarm(Long id) {
+        reminderRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(NOT_FIND_REMINDER_MESSAGE));
         alarmRepository.deleteAllByReminderReminderId(id);
     }
 
