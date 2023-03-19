a8597f7e29220d1e6ceac5f85e8cc18b0a354bb4
jum0624 <jum0624@naver.com>
2023-03-20 00:11:21 +0900

    refactor: Error Message code refactor

--- doormate/src/main/java/com/doormate/service/impl/ReminderService.java
+++ doormate/src/main/java/com/doormate/service/impl/ReminderService.java
@@ -20,6 +20,7 @@ public class ReminderService {
     private static final String SAVE_REMINDER_SUCCESS_MESSAGE = "리마인더 등록 완료";
     private static final String UPDATE_SUCCESS_MESSAGE = "리마인더 수정 완료";
     private static final String ERROR_NOT_EXISTS_REMINDER_MESSAGE = "해당 리마인더가 존재하지 않습니다.";
+    private static final String NOT_CORRECT_DATE_MESSAGE = "설정한 날짜의 범위를 다시 확인하세요.";
     private static final String DELETE_SUCCESS_MESSAGE = "리마인더 삭제 완료";
 
     @Transactional
@@ -52,10 +53,10 @@ public class ReminderService {
     }
 
     @Transactional
-    public Message deleteReminder(Long id) {
+    public String deleteReminder(Long id) {
         // 무결성 위반 방지를 위해 자식 테이블 값 삭제 후, 리마인더 삭제
         alarmRepository.deleteAllByReminderReminderId(id);
         reminderRepository.deleteById(id);
-        return new Message(DELETE_SUCCESS_MESSAGE);
+        return DELETE_SUCCESS_MESSAGE;
     }
 }
