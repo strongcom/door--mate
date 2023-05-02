2a407725e0a0e4c03b5693cb1f8cfb1333c837e1
jum0624 <jum0624@naver.com>
2023-05-03 01:41:22 +0900

    style: Add exception class, RepetitionDay 요일 필드 추가

--- doormate/src/main/java/com/doormate/domain/RepetitionDay.java
+++ doormate/src/main/java/com/doormate/domain/RepetitionDay.java
@@ -1,20 +1,22 @@
 package com.doormate.domain;
 
 public enum RepetitionDay {
-    MON(1, "MON"),
-    TUE(2, "TUE"),
-    WED(3, "WED"),
-    THUR(4, "THUR"),
-    FRI(5, "FRI"),
-    SAT(6, "SAT"),
-    SUN(7, "SUN");
+    MON(1, "MON", "월"),
+    TUE(2, "TUE", "화"),
+    WED(3, "WED", "수"),
+    THUR(4, "THUR", "목"),
+    FRI(5, "FRI", "금"),
+    SAT(6, "SAT", "토"),
+    SUN(7, "SUN", "일");
 
     private int number;
     private String day;
+    private String kor_day;
 
-    RepetitionDay(int number, String day) {
+    RepetitionDay(int number, String day, String kor_day) {
         this.number = number;
         this.day = day;
+        this.kor_day = kor_day;
     }
 
     public int getNumber() {
@@ -23,5 +25,20 @@ public enum RepetitionDay {
 
     public String getDay() {
         return day;
+    }
+
+    public String getKor_day(){ return kor_day; }
+
+    public static String toDay(String days) {
+        String[] split = days.split(" ");
+        String result = "";
+        for (String day : split
+        ) {
+            if (!result.equals("")) result += ", ";
+            RepetitionDay repetitionDay = RepetitionDay.valueOf(day);
+            String korDay = repetitionDay.getKor_day();
+            result += korDay;
+        }
+        return result;
     }
 }
