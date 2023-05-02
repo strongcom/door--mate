8b059cdbbb9341d11fa21462acfad825fd689e32
jum0624 <jum0624@naver.com>
2023-05-03 01:34:29 +0900

    refactor: 알람테이블 리펙터링
    유저와 연관관게 매핑 삭제 및 생성메서드, 비즈니스 로직 추가

--- doormate/src/main/java/com/doormate/domain/Alarm.java
+++ doormate/src/main/java/com/doormate/domain/Alarm.java
@@ -1,6 +1,8 @@
 package com.doormate.domain;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -8,22 +10,15 @@ import java.time.LocalTime;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Alarm {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long alarmId;
+    private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "reminder_id")
+    @JoinColumn
     private Reminder reminder;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
 
     @Column(nullable = false)
     private LocalDate noticeDate;
@@ -37,15 +32,14 @@ public class Alarm {
     /**
      * 연관관계 메서드
      */
-    // Alarm 내용이 생기면서 리마인더와 연관관계가 발생하여 외래키인 리마인더 id의 AlarmList로 연관관계 매핑
-    // 즉, 현재 저장되는 모든 알림내역들은 부모 테이블인 리마인더 객체를 외래키로 저장해줌.
     public void setReminder(Reminder reminder) {
-        if (this.reminder != null) {  // 현재 알람 객체의 리마인더 값이 null이 아니라면(이미 한번 등록된 이력이 있는 알림)
-            this.reminder.getAlarmList().remove(this);  // 해당 리마인더 외래키에서 제외시킴
+        if (this.reminder != null) {
+            this.reminder.getAlarms().remove(this);
         }
-        this.reminder = reminder;  // 현재 받은 리마인더 객체를 외래키로 저장
-        reminder.getAlarmList().add(this);  // 리마인더 테이블에도 알림에 대한 정보를 저장
+        this.reminder = reminder;
+        reminder.getAlarms().add(this);
     }
+
     @Builder
     public Alarm(Reminder reminder, LocalDate noticeDate, LocalTime startTime, LocalTime endTime) {
         setReminder(reminder);
@@ -53,4 +47,27 @@ public class Alarm {
         this.startTime = startTime;
         this.endTime = endTime;
     }
+
+
+    /**
+     * 생성 메서드
+     */
+    public static Alarm createAlarm(LocalDate date, Reminder reminder) {
+        return Alarm.builder()
+                .reminder(reminder)
+                .noticeDate(date)
+                .startTime(reminder.getStartTime())
+                .endTime(reminder.getEndTime())
+                .build();
+    }
+
+    /**
+     * 비즈니스 로직
+     */
+    public void setNoticeDate(LocalDate day){
+        this.noticeDate = day;
+    }
+
+
+
 }
